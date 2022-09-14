44e9260f21e8e425db71b62cce8f76e5fab93166
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-09-14 19:57:34 -0300

    4 - In progress - Car Controller

---
+++ src/controllers/CarController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import ICarService from '../interfaces/ICarService';
+import { ICar } from '../interfaces/ICar';
+
+export default class CarController {
+  constructor(private _service: ICarService<ICar>) {}
+
+  public async create(request: Request, response: Response<ICar>) {
+    try {
+      if (request.body.length < 1) return response.status(400).end();
+
+      const { buyValue, color, year, status, model, seatsQty, doorsQty } = request.body;
+
+      const car = { buyValue, color, year, status, model, seatsQty, doorsQty };
+
+      const results = await this._service.create(car);
+
+      return response.status(201).json(results);
+    } catch (error) {
+      return response.status(400).end();
+    }
+  }
+}
