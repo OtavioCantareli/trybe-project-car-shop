ab533a7d8ec905b2347782066de6c4eb6282e50d
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-09-14 19:54:50 -0300

    Added missing parameters

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -5,6 +5,12 @@ const zodCar = z.object({
   doorsQty: z.number().int().positive().gte(2)
     .lte(4),
   seatsQty: z.number().gte(2).lte(7),
+  model: z.string().min(3),
+  year: z.number().gte(1900).lte(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
 });
 
 export type ICar = z.infer<typeof zodCar> & IVehicle;
+export default zodCar;
