ed0ff9567a33d5f22c99aa31531b0ac133bd5219
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-09-14 18:07:20 -0300

    4 - In progress - Car Model

---
+++ src/models/CarModel.ts
@@ -0,0 +1,19 @@
+import { model as mongooseCreateModel, Schema } from 'mongoose';
+import { ICar } from '../interfaces/ICar';
+import MongoModel from './MongoModel';
+
+const carSchema = new Schema<ICar>({
+  buyValue: Number,
+  color: String,
+  year: Number,
+  model: String,
+  status: Boolean,
+  doorsQty: Number,
+  seatsQty: Number,
+});
+
+export default class Car extends MongoModel<ICar> {
+  constructor(model = mongooseCreateModel('Car', carSchema)) {
+    super(model);
+  }
+}
