0e67b90cc85f789edb874020ad3a432ca43527d8
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-09-15 12:49:52 -0300

    Mocks for tests

---
+++ src/tests/mocks/mocks.ts
@@ -0,0 +1,61 @@
+const modelCreate = {
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2,
+};
+
+const modelCreateWithId = {
+  _id: "4edd40c86762e0fb12000003",
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2,
+};
+
+const modelRead = [
+  {
+    _id: "4edd40c86762e0fb12000003",
+    model: "Ferrari Maranello",
+    year: 1963,
+    color: "red",
+    buyValue: 3500000,
+    seatsQty: 2,
+    doorsQty: 2,
+  },
+  {
+    _id: "4edd40c86762e0fb12000003",
+    model: "Ferrari Maranello",
+    year: 1963,
+    color: "blue",
+    buyValue: 3500000,
+    seatsQty: 4,
+    doorsQty: 2,
+  },
+];
+
+const modelReadOne = {
+  _id: "4edd40c86762e0fb12000003",
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2,
+};
+
+const modelRemove = {
+  _id: "4edd40c86762e0fb12000003",
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  seatsQty: 2,
+  doorsQty: 2,
+};
+
+export { modelCreate, modelCreateWithId, modelRead, modelReadOne, modelRemove };
