44e1564689e54041269c8ef50b3b7e8bcf974b5b
Otavio Cantareli <otavio_cantareli@outlook.com>
2022-09-15 20:19:14 -0300

    Added missing tests

--- src/tests/unit/controllers/CarController.test.ts
+++ src/tests/unit/controllers/CarController.test.ts
@@ -7,6 +7,7 @@ import CarModel from "../../../models/CarModel";
 import CarService from "../../../services/CarService";
 import CarController from "../../../controllers/CarController";
 import {
+  id,
   modelCreate,
   modelCreateWithId,
   modelRead,
@@ -43,5 +44,25 @@ describe("Car Controller", () => {
     expect((response.status as sinon.SinonStub).calledWith(201)).to.be.true;
     expect((response.json as sinon.SinonStub).calledWith(modelCreateWithId)).to
       .be.true;
+  });
+
+  describe("Reading", () => {
+    it("Read One", async () => {
+      request.params = { id };
+
+      await carController.readOne(request, response);
+
+      expect((response.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      expect((response.json as sinon.SinonStub).calledWith(modelReadOne)).to.be
+        .true;
+    });
+
+    it('Read All', async () => {
+      await carController.read(request, response);
+
+      expect((response.status as sinon.SinonStub).calledWith(200)).to.be.true;
+      expect((response.json as sinon.SinonStub).calledWith(modelRead)).to.be
+        .true;
+    })
   });
 });
